4b0fd2915733a1ffb5235dac7471719e4894e3bc
marclevetin <marc@derbyhat.org>
2018-05-08 00:02:13 -0400

    added filter and pagination feature

--- app.js
+++ app.js
@@ -1,54 +1,94 @@
-angular.module("app", [])
-    .controller("mainController", ($scope, getStarWars) => {
-        $scope.allPeople = [];
-        $scope.allPeopleCount = 0;
-        $scope.activePerson = {};
-        $scope.isThereMoreData = true;
-        
-        // this function gets people for the next page.
-        $scope.lastFetchedPage = 1;
-        $scope.getMorePeople = function(number) {
-            return getStarWars.getPeople(number)
-                        .then(data => {
-                            // this block of code adds fetch results to data set
-                            angular.forEach(data.results, function (person) {
-                                $scope.allPeople.push(person);
-                            });
-                            $scope.lastFetchedPage += 1;
+angular
+  .module("app", [])
+  .controller("mainController", ($scope, getStarWars) => {
+    $scope.allPeople = [];
+    $scope.activePerson = {};
+    $scope.isThereMoreData = true;
+    $scope.activePage = 1;
+    $scope.allPages = [];
 
-                            // this block disables the "next" button.
-                            if (data.next === null) {
-                                $scope.isThereMoreData = false;
-                            } 
+    // this function gets people for the next page.
+    $scope.lastFetchedPage = 1;
+    $scope.getMorePeople = function(number) {
+      $scope.allPages.push(number);
+      return getStarWars
+        .getPeople(number)
+        .then(data => {
+          // this block of code adds fetch results to data set
+          angular.forEach(data.results, function(person) {
+            $scope.allPeople.push(person);
+          });
+          $scope.lastFetchedPage += 1;
 
-                        })
-                        .catch((err) => { console.log(err) });
+          // this block disables the "next" button.
+          if (data.next === null) {
+            $scope.isThereMoreData = false;
+          }
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+
+    $scope.printToConsole = function() {
+      console.log($scope);
+    };
+
+    $scope.showMoreDetails = function(index) {
+      $scope.activePerson = $scope.allPeople[index];
+    };
+
+    $scope.clearActive = function() {
+      $scope.activePerson = {};
+    };
+
+    $scope.setActivePage = function(number) {
+      if (number === "Previous") {
+        $scope.activePage = $scope.activePage - 1;
+      } else if (number === "Next") {
+        $scope.activePage = $scope.activePage + 1;
+      } else {
+        $scope.activePage = number;
+      }
+    };
+
+    $scope.$watch("activePage", function(newVal, oldVal, scope) {
+      const havePageData = scope.allPages.includes(newVal);
+      if (!havePageData) {
+        // get new data
+        scope.getMorePeople(scope.activePage);
+      }
+    });
+
+    $scope.filterThings = function(activePage) {
+        return function (value, index, array) {
+            const maxIndex = activePage * 10 - 1;
+            const minIndex = (activePage - 1) * 10;
+
+            if (index >= minIndex && index <= maxIndex) {
+                return array[index]
+            } else {
+                return
+            }
         }
+    }
 
-        $scope.printToConsole = function() {
-            console.log($scope)
-        };
+    // this gets people the first time the page loads.
+    $scope.getMorePeople(1);
+  })
+  .service("getStarWars", function($http) {
+    this.getPeople = pageNumber => {
+      const url = "https://swapi.co/api/people/?page=" + pageNumber;
 
-        $scope.showMoreDetails = function(index) {
-            $scope.activePerson = $scope.allPeople[index];
-        }
-
-        $scope.clearActive = function() {
-            $scope.activePerson = {};
-        }
-
-        // this gets people the first time the page loads.
-        $scope.getMorePeople(1);
-    })
-    .service("getStarWars", function($http) {
-        this.getPeople = (pageNumber) => {
-            const url = 'https://swapi.co/api/people/?page=' + pageNumber;
-
-            return $http({
-                method: "GET",
-                url: url
-            }).then((data) => { return data.data })
-            .catch((err) => {console.log(err)});
-        }
-    })
-    
+      return $http({
+        method: "GET",
+        url: url
+      })
+        .then(data => {
+          return data.data;
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    };
+  })
